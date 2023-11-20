8b417c5e23580795e537a5cd0b98c59d65050eaa
aryan-212 <aryananandxic07@gmail.com>
2023-11-20 15:10:12 +0530

    hellothisisthe new on1

---
+++ src/HomePageAPI.js
@@ -0,0 +1,155 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+
+const apiResponse = {
+  slider: [
+    {
+      image:
+        "https://img.flixhq.to/xxrz/1200x600/379/ea/35/ea3599e89a8c6b4e966d1abab3ee2402/ea3599e89a8c6b4e966d1abab3ee2402.jpg",
+      title: "The Creator",
+      detail: [Object],
+      description:
+        "Amid a future war between the human race and the forces of artificial intelligence, a hardened ex-special forces agent grieving the disappearance of his wife...",
+      url: "https://flixhq.to/movie/watch-the-creator-100849",
+    },
+    {
+      image:
+        "https://img.flixhq.to/xxrz/1200x600/379/a5/c1/a5c1674ed733c43933cd11e877f050b5/a5c1674ed733c43933cd11e877f050b5.jpg",
+      title: "Loki",
+      detail: [Object],
+      description:
+        'After stealing the Tesseract during the events of "Avengers: Endgame." an alternate version of Loki is brought to the mysterious Time Variance Authority, a...',
+      url: "https://flixhq.to/tv/watch-loki-42258",
+    },
+    {
+      image:
+        "https://img.flixhq.to/xxrz/1200x600/379/05/45/05459ef32d9ace3b1fec571dfc08a8ce/05459ef32d9ace3b1fec571dfc08a8ce.jpg",
+      title: "Expend4bles",
+      detail: [Object],
+      description: "",
+      url: "https://flixhq.to/movie/watch-the-expendables-4-16861",
+    },
+    {
+      image:
+        "https://img.flixhq.to/xxrz/1200x600/379/6f/0d/6f0da2da38d52695d03be78f3e0a7045/6f0da2da38d52695d03be78f3e0a7045.jpg",
+      title: "Mission: Impossible - Dead Reckoning Part One",
+      detail: [Object],
+      description: "",
+      url: "https://flixhq.to/movie/watch-mission-impossible-7-68135",
+    },
+    {
+      image:
+        "https://img.flixhq.to/xxrz/1200x600/379/4c/ee/4cee1bdde99bf089d44e4b3dbb8399e4/4cee1bdde99bf089d44e4b3dbb8399e4.jpg",
+      title: "The Equalizer 3",
+      detail: [Object],
+      description:
+        "Robert McCall finds himself at home in Southern Italy but he discovers his friends are under the control of local crime bosses. As events turn deadly, McCall...",
+      url: "https://flixhq.to/movie/watch-the-equalizer-3-99865",
+    },
+    {
+      image:
+        "https://img.flixhq.to/xxrz/1200x600/379/85/09/8509eafa6b10694de8fe9027e415ecdf/8509eafa6b10694de8fe9027e415ecdf.jpg",
+      title: "The Nun II",
+      detail: [Object],
+      description:
+        "Set four years after the ending of the the nun, this follows Sister Irene as she investigates a murder at a boarding school in France. While investigating she...",
+      url: "https://flixhq.to/movie/watch-the-nun-ii-100063",
+    },
+    {
+      image:
+        "https://img.flixhq.to/xxrz/1200x600/379/e6/03/e60320897cc0cd1686cdaf6526dd436d/e60320897cc0cd1686cdaf6526dd436d.jpg",
+      title: "The Retirement Plan",
+      detail: [Object],
+      description:
+        "When Ashley and her young daughter Sarah get caught up in a criminal enterprise that puts their lives at risk, she turns to the only person who can help: her...",
+      url: "https://flixhq.to/movie/watch-the-retirement-plan-100294",
+    },
+    {
+      image:
+        "https://img.flixhq.to/xxrz/1200x600/379/e2/b5/e2b55f91a4e250e486f1a1084357fc1a/e2b55f91a4e250e486f1a1084357fc1a.jpg",
+      title: "Gran Turismo",
+      detail: [Object],
+      description:
+        "The ultimate wish fulfillment tale of a teenage Gran Turismo player whose gaming skills won him a series of Nissan competitions to become an actual professional...",
+      url: "https://flixhq.to/movie/watch-gran-turismo-98845",
+    },
+    {
+      image:
+        "https://img.flixhq.to/xxrz/1200x600/379/fe/74/fe7471ce85c019b6445563bfcf27c9b0/fe7471ce85c019b6445563bfcf27c9b0.jpg",
+      title: "Blue Beetle",
+      detail: [Object],
+      description:
+        "Recent college grad Jaime Reyes returns home full of aspirations for his future, only to find that home is not quite as he left it. As he searches to find his...",
+      url: "https://flixhq.to/movie/watch-blue-beetle-99433",
+    },
+    {
+      image:
+        "https://img.flixhq.to/xxrz/1200x600/379/31/f2/31f2df4f1ad949cf6e9b14ae4dda9c9c/31f2df4f1ad949cf6e9b14ae4dda9c9c.jpg",
+      title: "The Last Voyage of the Demeter",
+      detail: [Object],
+      description:
+        "The crew of the merchant ship Demeter attempts to survive the ocean voyage from Carpathia to London as they are stalked each night by a merciless presence...",
+      url: "https://flixhq.to/movie/watch-the-last-voyage-of-the-demeter-99376",
+    },
+    {
+      image:
+        "https://img.flixhq.to/xxrz/1200x600/379/c9/ac/c9ac99534c0ddb68d6ea82088913cc9c/c9ac99534c0ddb68d6ea82088913cc9c.jpg",
+      title: "Indiana Jones and the Dial of Destiny",
+      detail: [Object],
+      description:
+        "The plot is said to be based on one of the other books in the series.",
+      url: "https://flixhq.to/movie/watch-indiana-jones-5-9984",
+    },
+    {
+      image:
+        "https://img.flixhq.to/xxrz/1200x600/379/52/bf/52bf282a0f729fcee351085a1fb2adf7/52bf282a0f729fcee351085a1fb2adf7.jpg",
+      title: "Heart of Stone",
+      detail: [Object],
+      description:
+        "An intelligence operative for a shadowy global peacekeeping agency races to stop a hacker from stealing its most valuable — and dangerous — weapon.",
+      url: "https://flixhq.pe/movie/watch-heart-of-stone-99160",
+    },
+  ],
+};
+
+// Route to fetch movies
+app.get("/api/movies", (req, res) => {
+  // Here, you might send the apiResponse data as the response
+  res.json(apiResponse);
+});
+
+// Start the server
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+export default apiResponse;
+
+// // Function to extract arrays for specific keys
+// function extractArraysFromResponse(response) {
+//   const arrays = {};
+
+//   // Specify keys for which you want arrays
+//   const keysToExtract = ["image", "title", "url"];
+
+//   keysToExtract.forEach((key) => {
+//     arrays[key] = response.slider.map((item) => item[key]);
+//   });
+
+//   arrays["description"] = response.slider.map(
+//     (item) => item["description"] || ""
+//   );
+//   arrays["detail"] = response.slider.map((item) => item["detail"] || {});
+
+//   return arrays;
+// }
+
+// // Extract arrays for specific keys
+// const arraysForKeys = extractArraysFromResponse(apiResponse);
+
+// // Output the arrays for specific keys
+// console.log(arraysForKeys);
--- src/LoginPage.js
+++ src/LoginPage.js
@@ -1,94 +1,165 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
+import firebase from 'firebase/app';
+import 'firebase/auth';
 
-function LoginPage({ onClose }) {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
-  const [isSignUp, setIsSignUp] = useState(false);
+// Your Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCGzRdo4c1HC1qo1qxEfR_d0oRzSUcLllQ",
+  authDomain: "movieflix-5017b.firebaseapp.com",
+  projectId: "movieflix-5017b",
+  storageBucket: "movieflix-5017b.appspot.com",
+  messagingSenderId: "878710165881",
+  appId: "1:878710165881:web:4904171e810f86721e1fa8",
+  measurementId: "G-17FTV5CZ34"
+};
 
-  const handleSubmit = () => {
-    // Perform login or signup logic here (based on isSignUp state)
-    if (isSignUp) {
-      // Additional logic for signup
-      console.log("Sign up logic");
-      console.log("Username:", username);
-      console.log("Password:", password);
-      console.log("Confirm Password:", confirmPassword);
-    } else {
-      // Login logic
-      console.log("Login logic");
-      console.log("Username:", username);
-      console.log("Password:", password);
+// Initialize Firebase
+firebase.initializeApp( firebaseConfig );
+
+function LoginPage ( { onClose } )
+{
+  const [ username, setUsername ] = useState( '' );
+  const [ password, setPassword ] = useState( '' );
+  const [ confirmPassword, setConfirmPassword ] = useState( '' );
+  const [ isSignUp, setIsSignUp ] = useState( false );
+  const [ signedUpUsername, setSignedUpUsername ] = useState( null );
+
+  const handleSignup = async () =>
+  {
+    try
+    {
+      if ( password === confirmPassword )
+      {
+        await firebase.auth().createUserWithEmailAndPassword( username, password );
+        setSignedUpUsername( username );
+        setIsSignUp( false );
+      } else
+      {
+        console.error( 'Passwords do not match' );
+      }
+    } catch ( error )
+    {
+      console.error( 'Error signing up:', error.message );
     }
-
-    // Close the login page after submitting
-    onClose();
   };
 
-  const handleOverlayClick = (e) => {
-    // Close the login page if the overlay (outside the modal) is clicked
-    if (e.target === e.currentTarget) {
+  const handleLogin = async () =>
+  {
+    try
+    {
+      await firebase.auth().signInWithEmailAndPassword( username, password );
+      onClose();
+    } catch ( error )
+    {
+      console.error( 'Error logging in:', error.message );
+    }
+  };
+
+  const handleSignupSubmit = ( e ) =>
+  {
+    e.preventDefault();
+    handleSignup();
+  };
+
+  const handleLoginSubmit = ( e ) =>
+  {
+    e.preventDefault();
+    handleLogin();
+  };
+
+  const handleOverlayClick = ( e ) =>
+  {
+    if ( e.target === e.currentTarget )
+    {
       onClose();
     }
   };
 
-  const toggleMode = () => {
-    // Switch between login and signup modes
-    setIsSignUp((prevIsSignUp) => !prevIsSignUp);
+  const toggleMode = () =>
+  {
+    setIsSignUp( ( prevIsSignUp ) => !prevIsSignUp );
   };
 
   return (
     <div style={styles.overlay} onClick={handleOverlayClick}>
       <div style={styles.modal}>
         <div style={styles.modalContent}>
-          <h2>{isSignUp ? "Sign Up" : "Welcome back!"}</h2>
-          <h3></h3>
-          <label>
-            Username:
-            <input
-              type="text"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-              style={styles.input}
-            />
-          </label>
-          <br />
-          <label>
-            Password:
-            <input
-              type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              style={styles.input}
-            />
-          </label>
-          {isSignUp && (
-            <>
+          {signedUpUsername && (
+            <h4>
+              Welcome {signedUpUsername}
+              <br />
+              <br />
+              Confirm account creation?
+            </h4>
+          )}
+          {!signedUpUsername && <h2>{isSignUp ? 'Sign Up' : 'Welcome back!'}</h2>}
+          {isSignUp ? (
+            <form onSubmit={handleSignupSubmit}>
+              <label>
+                Username:
+                <input
+                  type="text"
+                  value={username}
+                  onChange={( e ) => setUsername( e.target.value )}
+                  style={styles.input}
+                />
+              </label>
+              <br />
+              <label>
+                Password:
+                <input
+                  type="password"
+                  value={password}
+                  onChange={( e ) => setPassword( e.target.value )}
+                  style={styles.input}
+                />
+              </label>
               <br />
               <label>
                 Confirm Password:
                 <input
                   type="password"
                   value={confirmPassword}
-                  onChange={(e) => setConfirmPassword(e.target.value)}
+                  onChange={( e ) => setConfirmPassword( e.target.value )}
                   style={styles.input}
                 />
               </label>
-            </>
+              <br />
+              <button type="submit" style={styles.greenButton}>
+                Sign Up
+              </button>
+            </form>
+          ) : (
+            <form onSubmit={handleLoginSubmit}>
+              <label>
+                Username:
+                <input
+                  type="text"
+                  value={username}
+                  onChange={( e ) => setUsername( e.target.value )}
+                  style={styles.input}
+                />
+              </label>
+              <br />
+              <label>
+                Password:
+                <input
+                  type="password"
+                  value={password}
+                  onChange={( e ) => setPassword( e.target.value )}
+                  style={styles.input}
+                />
+              </label>
+              <br />
+              <button type="submit" style={styles.greenButton}>
+                Submit
+              </button>
+            </form>
           )}
-          <br />
-          <div className="bg-green-500 border border-black rounded text-2l text-white text-center mt-2">
-            <button onClick={handleSubmit}>
-              {isSignUp ? "Sign Up" : "Submit"}
-            </button>
-          </div>
-          <div style={{ marginTop: "10px", color: "white" }}>
-            {isSignUp ? "Already have an account?" : "Don't have an account?"}{" "}
-            <span
-              onClick={toggleMode}
-              style={{ cursor: "pointer", color: "blue" }}
-            >
-              {isSignUp ? "Login now" : "Sign up now"}
+          <div style={{ marginTop: '10px', color: 'white' }}>
+            {isSignUp ? 'Already have an account?' : "Don't have an account?"}{' '}
+            <span onClick={toggleMode} style={{ cursor: 'pointer', color: 'blue' }}>
+              {isSignUp ? 'Login now' : 'Sign up now'}
             </span>
           </div>
         </div>
@@ -99,35 +170,43 @@ function LoginPage({ onClose }) {
 
 const styles = {
   overlay: {
-    position: "fixed",
+    position: 'fixed',
     top: 0,
     left: 0,
-    width: "100%",
-    height: "100%",
-    background: "rgba(0, 0, 0, 0.7)", // Adjust the alpha value for transparency
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
+    width: '100%',
+    height: '100%',
+    background: 'rgba(0, 0, 0, 0.7)',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
     zIndex: 1,
   },
   modal: {
-    backgroundColor: "black",
-    padding: "20px",
-    borderRadius: "8px",
-    textAlign: "center",
+    backgroundColor: 'black',
+    padding: '20px',
+    borderRadius: '8px',
+    textAlign: 'center',
   },
   modalContent: {
-    width: "300px", // Adjust the width as needed
+    width: '300px',
   },
   input: {
-    width: "100%",
-    padding: "8px",
-    margin: "5px 0",
-    boxSizing: "border-box",
-    backgroundColor: "black",
-    color: "white",
-    border: "1px solid white",
-    borderRadius: "4px",
+    width: '100%',
+    padding: '8px',
+    margin: '5px 0',
+    boxSizing: 'border-box',
+    backgroundColor: 'black',
+    color: 'white',
+    border: '1px solid white',
+    borderRadius: '4px',
+  },
+  greenButton: {
+    backgroundColor: 'green',
+    color: 'white',
+    padding: '8px',
+    border: 'none',
+    borderRadius: '4px',
+    cursor: 'pointer',
   },
 };
 
